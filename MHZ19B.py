57da894a3427602861bf58a9c56ba588af51d5d9
Albert Zakiev <albert.zakiev@gmail.com>
2024-10-27 22:53:36 +0300

    Fixed checksum calculation

--- MHZ19B.py
+++ MHZ19B.py
@@ -1,23 +1,13 @@
 # Class for reading data from MHZ19B sensor
 #
 # 2024-10-27 21:58:22
-#
-# Usage:
-# mhz19b = MHZ19B(uart_id=1, tx_pin=8, rx_pin=9)
-# while True:
-#     co2 = mhz19b.read_co2()
-#     if co2 is not None:
-#         print("CO2 Concentration:", co2, "ppm")
-#     else:
-#         print("Failed to read data from sensor")
-#     time.sleep(2)
 
 import machine
 import time
 
 class MHZ19B:
     def __init__(self, uart_id, tx_pin, rx_pin, baudrate=9600):
-        self.uart = machine.UART(uart_id, baudrate=baudrate, tx=tx_pin, rx=rx_pin)
+        self.uart = machine.UART(uart_id, baudrate=baudrate, tx=machine.Pin(tx_pin), rx=machine.Pin(rx_pin))
 
     def read_co2(self):
         request = bytearray([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79])
@@ -54,12 +44,15 @@ class MHZ19B:
         return
 
     def validate_response(self, response):
-        if len(response) != 9 or response[0] != 0xFF or response[1] != 0x86:
+        if response is None or len(response) != 9 or response[0] != 0xFF or response[1] != 0x86:
             return False
         
-        # Sum bytes from index 1 to 7 and compare with last byte
-        checksum = sum(response[1:8]) & 0xFF
-        if checksum == response[8]:
-            return True
-        
-        return False
+        i = 1
+        checksum = 0x00
+        while i < 8:
+            checksum += response[i] % 256
+            i += 1
+
+        checksum = ~checksum & 0xFF
+        checksum += 1
+        return checksum == response[8]
